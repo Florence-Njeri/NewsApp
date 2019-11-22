68ea36af75a90d9028df1874c8a020e24d2322f3
Florence-Njeri <flonsie17@gmail.com>
2019-11-22 12:06:45 +0300

    set up bottom navigation

---
+++ app/src/main/java/com/example/newsapp/ui/world/WorldFragment.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.ui.world
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.newsapp.R
+
+class WorldFragment : Fragment() {
+
+    private lateinit var worldViewModel: WorldViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        worldViewModel =
+            ViewModelProviders.of(this).get(WorldViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_world, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        worldViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
